e7856b3b638e9870a6229191dc5f375dbad2a99a
dodowujiayu <1501130808@qq.com>
2021-06-27 15:45:23 +0800

    human_detection

---
+++ dudu_Human_Detection/practise.py
@@ -0,0 +1,16 @@
+# leg = 5
+# ids = [i for i in range(leg)]
+# print('ewjdj:%s' % (ids))
+# class df(object):
+#     def __init__(self,nums):
+#         self.nums = nums
+#     def __getitem__(self,item):
+#         x = self.nums[item]
+#         return x
+# if __name__ == "__main__":
+#     nums = [1,2,4]
+#     a = df(nums)
+#     print(a.__getitem__(2))
+p = [1,2,3,4,5]
+
+print(p.index(1))
